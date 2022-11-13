98e59a7c0256819989ac252304e491d782e87e62
cheshire cat <45429645+ermii42@users.noreply.github.com>
2022-11-13 18:24:22 +0300

    Задачка про ежика филю

---
+++ src/last/task1/Main.java
@@ -0,0 +1,39 @@
+package last.task1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+// B. Филя и задание
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        int sm = 0;
+        ArrayList<Integer> numbers= new ArrayList<>();
+        for(int i=0; i<n;i++){
+            numbers.add(in.nextInt());
+            sm += numbers.get(i);
+        }
+
+        if(n==2){
+            System.out.println("YES");
+            return;
+        }
+        int middle = sm / n;
+        if(sm % n != 0){
+            middle+=1;
+        }
+        int current;
+        int k = Math.abs(numbers.get(0)-middle);
+        for (int i=1; i<n; i++){
+            current = Math.abs(numbers.get(i)-middle);
+            if(current !=0 && current !=k){
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+    }
+}
