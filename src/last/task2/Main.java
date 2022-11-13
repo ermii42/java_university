d9f07509e0db6f6c7ca8e164164453ae6fb5017b
cheshire cat <45429645+ermii42@users.noreply.github.com>
2022-11-13 20:44:57 +0300

    1143B Nirvana

--- src/last/task2/Main.java
+++ src/last/task2/Main.java
@@ -5,38 +5,12 @@ import java.util.Scanner;
 //№2. Nirvana
 public class Main {
     public static void main(String[] args) {
-        String n;
-        Scanner in = new Scanner(System.in);
-        //System.out.println('9'-'0');
-        n = in.next();
-        System.out.println(getMxMulty(n));
-
+        Scanner input=new Scanner(System.in);
+        long n=input.nextLong();
+        System.out.println(dfs(n));
     }
-
-    static int getMxMulty(String n){
-        int multy=1;
-        String s ="";
-        for(char s1: n.toCharArray()){
-            for(char s2: "9876543210".toCharArray()){
-                if(s1>=s2){
-                    s += s2;
-                    multy *= s2-'0';
-                    break;
-                }
-            }
-        }
-        int multy2=1;
-        String s_ ="";
-        for(char s1: "9".repeat(n.length()-1).toCharArray()){
-            for(char s2: "9876543210".toCharArray()){
-                if(s1>=s2){
-                    s += s2;
-                    multy2 *= s2-'0';
-                    break;
-                }
-            }
-        }
-        if(multy>multy2) return multy;
-        return multy2;
+    public static long dfs(long number){
+        if(number<10) return Math.max(number,1);
+        return Math.max(number%10*dfs(number/10), dfs(number/10-1)*9);
     }
 }
